7e30e37a6fc6b126aea567b8a216f81c3ad88269
Alesya <a.o.martsinkevich@gmail.com>
2022-03-02 21:39:57 +0300

    add new task

--- script.js
+++ script.js
@@ -11,46 +11,67 @@ genres
 
 P.S. Функции вызывать не обязательно*/
 
+
+// 1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
+// перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
+// Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
+// 
+// 2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
+// переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
+// 
+// 3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
+// Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
+// при помощи метода forEach вывести в консоль сообщения в таком виде:
+// "Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
+
+
+
 'use strict';
 
-// Код возьмите из предыдущего домашнего задания
 let numberOfFilms;
 
-function start(){
-    numberOfFilms = prompt('How many films do you watching', '');
-
-    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = prompt('How many films do you watching', '');
-    }
-
-}
 
 // start();
 
 
 const personelMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false,
-}
+    privat: true,
+    start: function () {
+        personelMovieDB.count = prompt('How many films do you watching', '');
 
-function showMyBD (hidden){
-    if(personelMovieDB.privat == false){
-        console.log(personelMovieDB);
-    } 
-}
-
-showMyBD(personelMovieDB.privat);
-
-
-
-
-function writeYourGenres(){
-    for(let i = 1; i <= 3; i++){
-        personelMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`) ;;
+        while(personelMovieDB.count == '' || personelMovieDB.count == null || isNaN(personelMovieDB.count)) {
+        personelMovieDB.count = prompt('How many films do you watching', '');
     }
+    },
+    writeYourGenres: function(){
+        for(let i = 1; i <= 3; i++){
+            personelMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+            while(personelMovieDB.genres === '' || personelMovieDB.genres === null) {
+                personelMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+            }
+        }
+
+
+        //здесяяяяяя
+    },
+    showMyBD: function(){
+        if(personelMovieDB.privat == false){
+            console.log(personelMovieDB);
+        } 
+    },
+    toggleVisibleMyDB: function(){
+        if(personelMovieDB.privat) {
+            personelMovieDB.privat = false;
+        } else {
+            personelMovieDB.privat = true;
+        }
+    }
+
 }
 
-writeYourGenres();
+personelMovieDB.toggleVisibleMyDB();
+personelMovieDB.showMyBD();
